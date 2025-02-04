506990717acbb1d1eac1d13fc48681e76b341b5d
UWKSATech <uofwaterlooksa@gmail.com>
2025-02-03 19:25:07 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     )
     app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's dynamic port
+    app.run(host="0.0.0.0", port=port)
